a70ce64d8a5fa43e080861e4d56948063c3481d3
snt23skv <snt_sk@mail.ru>
2021-03-12 16:22:56 +0400

    create: added review section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,19 +1,44 @@
-const swiper = new Swiper(".swiper-container", {
+const hotelSlider = new Swiper(".hotel-slider", {
 	// Optional parameters
 	direction: "horizontal",
 	loop: true,
+	speed: 2000,
+	effect: "fade",
 
 	// Navigation arrows
 	navigation: {
-		nextEl: ".slider-button--next",
-		prevEl: ".slider-button--prev",
+		nextEl: ".hotel-slider__button--next",
+		prevEl: ".hotel-slider__button--prev",
 	},
 });
 document.addEventListener("keydown", function (event) {
 	if (event.code == "ArrowLeft") {
-		swiper.slidePrev();
+		hotelSlider.slidePrev();
 	}
 	if (event.code == "ArrowRight") {
-		swiper.slideNext();
+		hotelSlider.slideNext();
+	}
+});
+
+const reviewsSlader = new Swiper(".reviews-slider", {
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+	speed: 500,
+	effect: "slide",
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".reviews-slider__button--next",
+		prevEl: ".reviews-slider__button--prev",
+	},
+});
+
+document.addEventListener("keydown", function (event) {
+	if (event.code == "ArrowLeft") {
+		reviewsSlader.slidePrev();
+	}
+	if (event.code == "ArrowRight") {
+		reviewsSlader.slideNext();
 	}
 });
